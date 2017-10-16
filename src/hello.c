0b3ff20b30305da8ca5776bc7a626dc7017aecb4
Marlin- <marlin-@gmx.cn>
2017-10-16 11:25:16 +0800

    Modify the debug messages

--- src/hello.c
+++ src/hello.c
@@ -22,10 +22,10 @@ SEXP c_ascii_art_str() {
 
 static void _basic_heap_finalizer(SEXP ext) {
     if (NULL == R_ExternalPtrAddr(ext)) {
-        Rprintf("Debug: empty ptr (_basic_heap_finalizer)\n");
+        Rprintf("_basic_heap_finalizer: Empty ptr\n");
         return;
     }
-    Rprintf("Debug: finalizing (_basic_heap_finalizer)\n");
+    Rprintf("_basic_heap_finalizer: Finalizing\n");
     basic_struct* symbol = (basic_struct*) R_ExternalPtrAddr(ext);
     basic_free_heap(symbol);
     R_ClearExternalPtr(ext);
@@ -33,6 +33,8 @@ static void _basic_heap_finalizer(SEXP ext) {
 
 SEXP c_new_heap_symbol(SEXP RString) {
     const char* str_symbol = CHAR(Rf_asChar(RString));
+    
+    Rprintf("c_new_heap_symbol: The extracted string is '%s'\n", str_symbol);
     
     basic_struct* symbol = basic_new_heap();
     symbol_set(symbol, str_symbol);
