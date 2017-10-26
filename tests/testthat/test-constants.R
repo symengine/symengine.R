d8b4ce95ad8ae75170da92bd6ad4999645b9d652
Marlin- <marlin-@gmx.cn>
2017-10-26 14:07:47 +0800

    Test for builtin constants

---
+++ tests/testthat/test-constants.R
@@ -0,0 +1,52 @@
+context("Test Constants")
+
+test_that("Error in `api_builtin_const`", {
+    expect_error(regexp = "Not a builtin constant", {
+        api_builtin_const("inf")
+    })
+})
+
+test_that("Builtin constants matching its str", {
+    check_const_match_str <- function (from, to) {
+        local({
+            # Using `api_builtin_const`
+            ptr <- api_builtin_const(from)
+            res <- api_basic_str(ptr)
+            
+            expect_identical(res, to)
+        })
+        
+        local({
+            # Using `get_builtin_const`
+            s   <- get_builtin_const(from)
+            res <- api_basic_str(s@.xData)
+            
+            expect_identical(res, to)
+        })
+        
+        invisible(TRUE)
+    }
+    pairs <- c(
+        "zero"         = "0"            ,
+        "one"          = "1"            ,
+        "minus_one"    = "-1"           ,
+        "I"            = "I"            ,
+        "pi"           = "pi"           ,
+        "E"            = "E"            ,
+        "EulerGamma"   = "EulerGamma"   ,
+        "Catalan"      = "Catalan"      ,
+        "GoldenRatio"  = "GoldenRatio"  ,
+        "Inf"          = "oo"           ,
+        "NegInf"       = "-oo"          ,
+        "ComplexInf"   = "zoo"          ,
+        "Nan"          = "nan"
+    )
+    
+    for (i in seq_along(pairs))
+        check_const_match_str(names(pairs)[[i]], pairs[[i]])
+    
+})
+
+test_that("Custom constants", {
+    # TODO
+})
