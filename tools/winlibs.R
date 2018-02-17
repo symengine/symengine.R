86772e311cb9c25a246a89d14458cc634be5359f
Marlin- <marlin-@gmx.cn>
2018-02-18 01:15:29 +0800

    Script for downloading binaries

---
+++ tools/winlibs.R
@@ -0,0 +1,17 @@
+
+if (!file.exists("../windows/usr/include/symengine/cwrapper.h")) {
+    machine <- Sys.info()[["machine"]]
+
+    if (machine == "x86_64")
+        link <- "https://github.com/symengine/symengine/releases/download/v0.3.0/binaries-msvc-x86_64.tar.bz2"
+    else if (machine == "x86")
+        link <- "https://github.com/symengine/symengine/releases/download/v0.3.0/binaries-msvc-x86.tar.bz2"
+    else
+        stop("Unrecognized type of machine, which should be either 'x86' or 'x86_64' ??")
+
+    download.file(link, "lib.tar.bz2", quiet = FALSE)
+    dir.create("../windows")
+    untar("lib.tar.bz2", exdir = "../windows")
+    unlink("lib.tar.bz2")
+}
+
