ebd8fab0792ab5f837b4fa6ed18674845cfe7fbc
Marlin- <marlin-@gmx.cn>
2018-02-19 06:09:02 +0800

    Update test

--- tests/testthat/test-basic.R
+++ tests/testthat/test-basic.R
@@ -3,15 +3,22 @@ context("Basic")
 test_that("Symbol", {
     x <- Symbol("x")
     expect_identical(basic_type(x), "Symbol")
+    expect_identical(basic_str(x),  "x")
+    
     x <- Symbol("")
     expect_identical(basic_type(x), "Symbol")
+    expect_identical(basic_str(x),  "")
+    
     x <- Symbol(NA_character_)
     expect_identical(basic_type(x), "Symbol")
-    x <- Symbol(42L)
-    expect_identical(basic_type(x), "Symbol")
+    expect_identical(basic_str(x),  "NA")
+    
     x <- Symbol(Inf)
     expect_identical(basic_type(x), "Symbol")
+    expect_identical(basic_str(x),  "Inf")
+    
     x <- Symbol(NaN)
     expect_identical(basic_type(x), "Symbol")
+    expect_identical(basic_str(x),  "NaN")
     
 })
