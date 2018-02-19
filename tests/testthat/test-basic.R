64af9aa1780a799f650045daa70a0aa05fa79ac5
Marlin- <marlin-@gmx.cn>
2018-02-19 10:40:24 +0800

    Add a test on constant

--- tests/testthat/test-basic.R
+++ tests/testthat/test-basic.R
@@ -22,3 +22,31 @@ test_that("Symbol", {
     expect_identical(basic_str(x),  "NaN")
     
 })
+
+test_that("Constants", {
+    #expect_true(basic_I() == S(1i))
+    expect_true(basic_I()  == S("1I"))
+    
+    expect_true(basic_pi() == S("pi"))
+    expect_true(basic_pi() == Constant("pi"))
+    
+    expect_true(basic_E() == S("E"))
+    expect_true(basic_E() == Constant("E"))
+    
+    expect_true(basic_EulerGamma() == S("EulerGamma"))
+    expect_true(basic_EulerGamma() == Constant("EulerGamma"))
+    
+    expect_true(basic_Catalan() == S("Catalan"))
+    expect_true(basic_Catalan() == Constant("Catalan"))
+    
+    expect_true(basic_GoldenRatio() == S("GoldenRatio"))
+    expect_true(basic_GoldenRatio() == Constant("GoldenRatio"))
+    
+    # TODO:
+    #expect_true(basic_infinity() == S(Inf))
+    #expect_true(basic_neginfinity() == S(-Inf))
+    #expect_true(basic_complex_infinity() == ???)
+    #expect_true(basic_nan() = ???)
+})
+
+
