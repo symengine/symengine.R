6597aed0394dcbd8f8092e539aae9f81fb95156c
Marlin- <marlin-@gmx.cn>
2019-05-20 05:40:37 -0700

    Add windows support

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,39 @@
 
+## We do not use "useDynLib" in NAMESPACE but control it in
+## .onLoad() and .onUnload() manually.
+## This gives us the flexibility to behave differently on windows
+## and unix. On windows, we put additional dlls to the package
+## folder and load them via `library.dynam` at run time. On Unix,
+## we assume that the dependencies such as gmp are in system path.
+
+.onLoad <- function(libname, pkgname) {
+    if (.Platform$OS.type == "windows")
+        return(.onLoad.windows(libname, pkgname))
+    
+    library.dynam("symengine", "symengine", libname)
+}
+
+.onUnload <- function(libpath) {
+    if (.Platform$OS.type == "windows")
+        return(.onUnload.windows(libpath))
+    
+    library.dynam.unload("symengine", libpath)
+}
+
+.onLoad.windows <- function(libname, pkgname) {
+    library.dynam("libgmp-10"   , "symengine", libname)
+    ## Currently using static library as workaround
+    #library.dynam("libsymengine", "symengine", libname)
+    library.dynam("symengine"   , "symengine", libname)
+}
+
+.onUnload.windows <- function(libpath) {
+    library.dynam.unload("symengine"   , libpath)
+    ## Currently using static library as workaround
+    #library.dynam.unload("libsymengine", libpath)
+    library.dynam.unload("libgmp-10"   , libpath)
+}
+
 .onAttach <- function (libname, pkgname) {
     # TODO:
     # Check whether symengine is available, and give verbose message if not.
@@ -28,6 +63,3 @@
     invisible(TRUE)
 }
 
-.onUnload <- function (libpath) {
-    library.dynam.unload("symengine", libpath)
-}
---
+++ tools/winlibs.R
@@ -0,0 +1,66 @@
+
+## Ensure we are in the correct directory
+stopifnot(file.exists("../DESCRIPTION"))
+
+## Links
+symengine_win32 <- "https://github.com/Marlin-Na/symengine/raw/r-dep2/symengine-32.zip"
+symengine_win64 <- "https://github.com/Marlin-Na/symengine/raw/r-dep2/symengine-64.zip"
+gmp_win32       <- "https://github.com/Marlin-Na/symengine/raw/r-dep2/gmp-32.zip"
+gmp_win64       <- "https://github.com/Marlin-Na/symengine/raw/r-dep2/gmp-64.zip"
+
+is_32 <- function () {
+    machine <- Sys.info()[["machine"]]
+    
+    if (machine %in% c("x86_64", "x86-64"))
+        use_32 <- FALSE
+    else if (machine == "x86")
+        use_32 <- TRUE
+    else
+        stop("Unrecognized type of machine ", sQuote(machine))
+}
+
+## These functions will download the binaries to package directory and move
+## the dll files to `inst/libs/{r_arch}/`.
+download_lib <- function(link, download_dist) {
+    ## Clean and create directory for compiling
+    if (dir.exists(download_dist))
+        unlink(download_dist, recursive = TRUE)
+    dir.create(download_dist)
+    
+    ## Create directory to put DLLs
+    r_arch <- .Platform$r_arch
+    lib_dist <- sprintf("../inst/libs/%s", r_arch)
+    if (!dir.exists(lib_dist))
+        dir.create(lib_dist, recursive = TRUE)
+    
+    ## Download files
+    download.file(link, destfile=file.path(download_dist, "download.zip"), quiet=FALSE)
+    unzip(file.path(download_dist, "download.zip"), exdir=download_dist)
+    unlink(file.path(download_dist, "download.zip"))
+    
+    ## TODO: do we need to rename libsymengine.dll?
+    ## Put DLLs
+    for (dll in list.files(file.path(download_dist, "bin"), full.names=TRUE)) {
+        file.rename(dll, file.path(lib_dist, basename(dll)))
+        message(sprintf("Put %s", basename(dll)))
+    }
+    invisible(TRUE)
+}
+
+
+
+download_symengine <- function() {
+    link <- if(is_32()) symengine_win32 else symengine_win64
+    download_dist <- "../win-symengine"
+    download_lib(link, download_dist)
+}
+
+download_gmp <- function() {
+    link <- if(is_32()) gmp_win32 else gmp_win64
+    download_dist <- "../win-gmp"
+    download_lib(link, download_dist)
+}
+
+
+download_symengine()
+download_gmp()
