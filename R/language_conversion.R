f5a7c88e20f039278842f5ef41c955cc01e64897
Jialin Ma <marlin@inventati.org>
2019-08-01 10:47:19 -0700

    Expand asLanguageTable

--- R/language_conversion.R
+++ R/language_conversion.R
@@ -11,12 +11,18 @@ asLanguage <- function(x) {
     func(x)
 }
 
+# A reference can be:
+# https://github.com/symengine/symengine.py/blob/master/symengine/lib/symengine_wrapper.pyx#L35
 asLanguageTable <- as.environment(list(
     Symbol     = function(s) as.name(as.character(s)),
     Integer    = function(s) as.integer(s),
     RealDouble = function(s) as.double(s),
     Constant   = function(s) as.double(evalf(s)),
     Rational   = function(s) as.double(evalf(s)),
+    `NaN`      = function(s) NaN,
+    #Complex    = function(s) stop("TODO"),
+    #ComplexDouble = function(s) stop("TODO"),
+    #ComplexMPC = function(s) stop("TODO"),
     Add = function(s) {
         Reduce(x = as.list(s4basic_get_args(s)),
                function(a, b) bquote(.(asLanguage(a)) + .(asLanguage(b))))
@@ -34,7 +40,51 @@ asLanguageTable <- as.environment(list(
         if (s4basic_number_is_positive(s)) return(Inf)
         else if (s4basic_number_is_negative(s)) return(-Inf)
         stop("Unexpected")
-    }
+    },
+    Abs   = function(s) bquote(sin(   .(asLanguage(s4basic_get_args(s)[[1]])) )),
+    Log   = function(s) bquote(log(   .(asLanguage(s4basic_get_args(s)[[1]])) )),
+    Gamma = function(s) bquote(gamma( .(asLanguage(s4basic_get_args(s)[[1]])) )),
+    Sin   = function(s) bquote(sin(   .(asLanguage(s4basic_get_args(s)[[1]])) )),
+    Cos   = function(s) bquote(cos(   .(asLanguage(s4basic_get_args(s)[[1]])) )),
+    Tan   = function(s) bquote(tan(   .(asLanguage(s4basic_get_args(s)[[1]])) )),
+    ASin  = function(s) bquote(asin(  .(asLanguage(s4basic_get_args(s)[[1]])) )),
+    ACos  = function(s) bquote(acos(  .(asLanguage(s4basic_get_args(s)[[1]])) )),
+    ATan  = function(s) bquote(atan(  .(asLanguage(s4basic_get_args(s)[[1]])) )),
+    Sinh  = function(s) bquote(sinh(  .(asLanguage(s4basic_get_args(s)[[1]])) )),
+    Cosh  = function(s) bquote(cosh(  .(asLanguage(s4basic_get_args(s)[[1]])) )),
+    Tanh  = function(s) bquote(tanh(  .(asLanguage(s4basic_get_args(s)[[1]])) )),
+    ASinh = function(s) bquote(asinh( .(asLanguage(s4basic_get_args(s)[[1]])) )),
+    ACosh = function(s) bquote(acosh( .(asLanguage(s4basic_get_args(s)[[1]])) )),
+    ATanh = function(s) bquote(atanh( .(asLanguage(s4basic_get_args(s)[[1]])) )),
+    #Cot   = function(s) stop("TODO"),
+    #Csc   = function(s) stop("TODO"),
+    #Sec   = function(s) stop("TODO"),
+    #ACot  = function(s) stop("TODO"),
+    #ACsc  = function(s) stop("TODO"),
+    #ASec  = function(s) stop("TODO"),
+    #Coth  = function(s) stop("TODO"),
+    #Csch  = function(s) stop("TODO"),
+    #Sech  = function(s) stop("TODO"),
+    #ACoth = function(s) stop("TODO"),
+    #ACsch = function(s) stop("TODO"),
+    #ASech = function(s) stop("TODO"),
+    ATan2 = function(s) {
+        args <- s4basic_get_args(s)
+        stop("TODO")
+    },
+    LambertW = function(s) bquote(lambertw( .(asLanguage(s4basic_get_args(s)[[1]])) )),
+    Zeta = function(s) {
+        ## Zeta has two args, ensure the second arg is One
+        args <- s4basic_get_args(s)
+        stopifnot(args[[2L]] == S(1L))
+        bquote(zeta( .(asLanguage(args[[1]])) ))
+    },
+    Dirichlet_eta = function(s) bquote(dirichlet_eta( .(asLanguage(s4basic_get_args(s)[[1]])) )),
+    Erf      = function(s) bquote(erf(    .(asLanguage(s4basic_get_args(s)[[1]])) )),
+    Erfc     = function(s) bquote(erfc(   .(asLanguage(s4basic_get_args(s)[[1]])) )),
+    LogGamma = function(s) bquote(lgamma( .(asLanguage(s4basic_get_args(s)[[1]])) ))
+    #KroneckerDelta, LeviCivita, LowerGamma, UpperGamma,
+    #Beta, PolyGamma, Sign, Floor, Ceiling,
 ))
 
 
