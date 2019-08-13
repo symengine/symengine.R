43a0e2104885eaa476a702f03ffcbd3aef2de36c
Jialin Ma <marlin@inventati.org>
2019-08-12 23:41:40 -0700

    Fix tests

--- tests/testthat/test-double_visitor.R
+++ tests/testthat/test-double_visitor.R
@@ -2,7 +2,8 @@ context("Double Visitor")
 
 test_that("LambdaDoubleVisitor for Basic", {
     expr <- S("x - y")
-    f <- LambdaDoubleVisitor(expr, args = Vector("x", "y"))
+    f <- DoubleVisitor(expr, args = Vector("x", "y"), llvm_opt_level = -1L)
+    expect_true(class(f) == "LambdaDoubleVisitor")
     res <- f(y = 1:10, x = 1)
     expect_identical(res, as.numeric(0:-9))
     
@@ -10,7 +11,8 @@ test_that("LambdaDoubleVisitor for Basic", {
 
 test_that("LambdaDoubleVisitor for VecBasic", {
     expr <- V("x + y", "x - y")
-    f <- LambdaDoubleVisitor(expr)
+    f <- DoubleVisitor(expr, llvm_opt_level = -1L)
+    expect_true(class(f) == "LambdaDoubleVisitor")
     res <- f(x = c(1,2,3,4), y = c(4,3,2,1))
     expect_true(class(res) == "matrix")
     expect_identical(dim(res), c(2L, 4L))
