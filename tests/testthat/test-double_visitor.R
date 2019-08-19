e5a084bdc5090800d9c57ba0e06d554f65386f78
Jialin Ma <marlin@inventati.org>
2019-08-18 21:08:28 -0400

    Fix tests for DoubleVisitor

--- tests/testthat/test-double_visitor.R
+++ tests/testthat/test-double_visitor.R
@@ -15,7 +15,7 @@ test_that("LambdaDoubleVisitor for VecBasic", {
     expect_true(class(f) == "LambdaDoubleVisitor")
     res <- f(x = c(1,2,3,4), y = c(4,3,2,1))
     expect_true(class(res) == "matrix")
-    expect_identical(dim(res), c(2L, 4L))
-    expect_identical(res[1, ], rep(5, 4))
+    expect_identical(dim(res), c(4L, 2L))
+    expect_identical(res[, 1], rep(5, 4))
 })
 
