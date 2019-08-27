fe1d3f3b7724b4766b7cddc187237441e15f47a8
Jialin Ma <marlin-@gmx.cn>
2019-08-26 20:46:39 -0400

    Add tests for LLVMDoubleVisitor

--- tests/testthat/test-double_visitor.R
+++ tests/testthat/test-double_visitor.R
@@ -19,3 +19,26 @@ test_that("LambdaDoubleVisitor for VecBasic", {
     expect_identical(res[, 1], rep(5, 4))
 })
 
+
+test_that("LLVMDoubleVisitor for Basic", {
+    skip_if_not(symengine_have_component("llvm"))
+    
+    expr <- S("x - y")
+    f <- DoubleVisitor(expr, args = Vector("x", "y"), llvm_opt_level = 1L)
+    expect_true(class(f) == "LLVMDoubleVisitor")
+    res <- f(y = 1:10, x = 1)
+    expect_identical(res, as.numeric(0:-9))
+    
+})
+
+test_that("LLVMDoubleVisitor for VecBasic", {
+    skip_if_not(symengine_have_component("llvm"))
+    
+    expr <- V("x + y", "x - y")
+    f <- DoubleVisitor(expr, llvm_opt_level = 1L)
+    expect_true(class(f) == "LLVMDoubleVisitor")
+    res <- f(x = c(1,2,3,4), y = c(4,3,2,1))
+    expect_true(class(res) == "matrix")
+    expect_identical(dim(res), c(4L, 2L))
+    expect_identical(res[, 1], rep(5, 4))
+})
