12f93d1b4e591b6234cb1ad5cc569dca2b4b0b75
Jialin Ma <marlin@inventati.org>
2019-11-26 03:41:03 -0500

    Fix test
    related info:
    https://developer.r-project.org/blosxom.cgi/R-devel/NEWS/2019/11/22#n2019-11-22

--- tests/testthat/test-double_visitor.R
+++ tests/testthat/test-double_visitor.R
@@ -14,11 +14,10 @@ test_that("LambdaDoubleVisitor for VecBasic", {
     f <- DoubleVisitor(expr, llvm_opt_level = -1L)
     expect_true(class(f) == "LambdaDoubleVisitor")
     res <- f(x = c(1,2,3,4), y = c(4,3,2,1))
-    expect_true(class(res) == "matrix")
+    expect_true(is.matrix(res))
     expect_identical(dim(res), c(4L, 2L))
     expect_identical(res[, 1], rep(5, 4))
 })
-
 
 test_that("LLVMDoubleVisitor for Basic", {
     skip_if_not(symengine_have_component("llvm"))
@@ -38,7 +37,7 @@ test_that("LLVMDoubleVisitor for VecBasic", {
     f <- DoubleVisitor(expr, llvm_opt_level = 1L)
     expect_true(class(f) == "LLVMDoubleVisitor")
     res <- f(x = c(1,2,3,4), y = c(4,3,2,1))
-    expect_true(class(res) == "matrix")
+    expect_true(is.matrix(res))
     expect_identical(dim(res), c(4L, 2L))
     expect_identical(res[, 1], rep(5, 4))
 })
