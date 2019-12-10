a3707e458448c74073456c0a0775cf3f44aa30c8
Jialin Ma <marlin@inventati.org>
2019-12-09 22:17:01 -0500

    Fix when missing diffstat

--- tools/bundle_symengine_source.sh
+++ tools/bundle_symengine_source.sh
@@ -27,7 +27,8 @@ rm -r symengine_source
 
 tput setaf 3; echo === Apply patch; tput sgr0
 
-diffstat -p0 symengine_patch.diff
+# diffstat may not be available
+diffstat -p0 symengine_patch.diff || true
 patch -p0 <symengine_patch.diff
 
 tput setaf 3; echo === Remove some unnecessary files in src/upstream; tput sgr0
